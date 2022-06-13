042d93d19bf58cc2d5b5d1dec80ba59747ccb7bc
Ken <67538811+YinHk@users.noreply.github.com>
2022-06-13 01:38:47 -0400

    Update Axios.js

--- cheatsheet/Axios.js
+++ cheatsheet/Axios.js
@@ -1,21 +1,26 @@
+const axios = require("axios");
+const url = "https://jexample.com";
+
 /* Making a get request */
 
 //With Promises (without async/await)
-const axios = require("axios");
-const url = "https://jsonplaceholder.typicode.com/todos/1";
-
 axios.get(url)
   .then((response) => response)
   .then((responseObject) => console.log(responseObject.data))
   .catch((err) => console.log(err));
 
 //With async/await
-const axios = require("axios");
 const getData = async (url) => {
   const res = await axios.get(url);
   const json = await res.data;
   console.log(json);
 };
-
-const url = "https://jsonplaceholder.typicode.com/todos/1";
 getData(url);
+
+//axios try catch
+try {
+  let res = await axios.get('/bad-call');
+  let data = res.data;
+} catch (error) {
+    console.log(error);    
+}
